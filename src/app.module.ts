51fe58580ce12fefed501a7aeb9cebc002ce5b91
unknown <bima.tn.dev.h.f@gmail.com>
2024-08-12 08:30:23 +0200

    'firstnest'

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [  TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+      }),
+      ConfigModule.forRoot(),UserModule,],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,57 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate,OneToMany } from 'typeorm';
+import { UserRole } from './role'; // Import UserRole enum
+import { Agency } from 'src/agency/entities/agency.entity';
+import { Randonnee } from 'src/random/entities/random.entity';
+import { Reservation } from 'src/reservation/entities/reservation.entity';
+import { Client } from 'src/client/entities/client.entity';
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: number;
+  @Column({name:" name", unique: true })
+  username: string;
+  @Column( "text",{name:"password" ,nullable: true })
+  password: string;
+  @Column("text" ,{ name:'email',nullable: true })
+  email: string;
+  @Column("text",{ name:'firstName' ,nullable: true })
+  firstName: string;
+  @Column("text",{ name:'lastName' ,nullable: true })
+  lastName: string;
+  // @Column({
+  //   type: 'enu',
+  //   enum: UserRole,
+  //   default: UserRole.USER,
+  // })
+  @Column("text",{ name:'role' ,nullable: true })
+  role: string;
+  @Column("date",{  nullable: true })
+  createdAt: Date;
+  @Column("date",{  nullable: true })
+  updatedAt: Date;
+  @Column("boolean",{  nullable: true })
+  isActive: boolean;
+  @Column("text",{  nullable: true })
+  tokenValue: string;
+  // @OneToMany(() => Agency, (agency: Agency) => agency.userId, { cascade: true})
+  // agencies: Agency[];
+  @OneToMany(() => Randonnee, (randonne: Randonnee) => randonne.userId, { cascade: true})
+  randonnees: Randonnee[]
+  @OneToMany(() => Reservation, (reservation: Reservation) => reservation.clientId, { cascade: true})
+  reservations: Reservation[]
+
+  @OneToMany(() => Client, (client: Client) => client.userId, { cascade: true})
+  Clients: Client[]/// class User
+
+
+  @BeforeInsert()
+  eventCreatedAt() {
+    this.createdAt = new Date();
+  }
+  @BeforeUpdate()
+  eventUpdatedAt() {
+    this.updatedAt = new Date();
+  }
+}
+
